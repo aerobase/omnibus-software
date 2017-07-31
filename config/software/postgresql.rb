48214c4d28ff15cab760641912e4b9a79b449c2f
Yaniv Marom Nachumi <yanivn@c-b4.local>
2017-07-31 13:17:26 +0300

    Update postgresql version

--- config/software/postgresql.rb
+++ config/software/postgresql.rb
@@ -15,7 +15,7 @@
 #
 
 name "postgresql"
-default_version "9.2.24" # NOTE: This version is EoL, but many downstream users take the default, and we shouldn't break them
+default_version "9.6.10"
 
 license "PostgreSQL"
 license_file "COPYRIGHT"
