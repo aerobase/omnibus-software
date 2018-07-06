3ece0ee7da54c596540b05933076d562d86d68c9
Yaniv Marom Nachumi <yaniv@aerobase.org>
2018-07-06 22:42:06 +0300

    Use aerobase omnibus-ctl master version

--- config/software/omnibus-ctl.rb
+++ config/software/omnibus-ctl.rb
@@ -15,7 +15,7 @@
 #
 
 name "omnibus-ctl"
-default_version "v0.5.0"
+default_version "master"
 
 license "Apache-2.0"
 license_file "https://raw.githubusercontent.com/chef/omnibus-ctl/master/LICENSE"
@@ -26,7 +26,7 @@ dependency "ruby"
 dependency "rubygems"
 dependency "bundler"
 
-source git: "https://github.com/chef/omnibus-ctl.git"
+source git: "https://github.com/aerobase/omnibus-ctl.git"
 
 relative_path "omnibus-ctl"
 
