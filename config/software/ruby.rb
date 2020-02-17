975a5e82372c575c12ac2e33d9a2cc96bb0f2163
Yaniv Marom Nachumi <yaniv@aerobase.org>
2020-02-18 00:21:25 +0200

    Fix build failure with omnibus 6.x

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -71,15 +71,6 @@ source url: "https://cache.ruby-lang.org/pub/ruby/#{version.match(/^(\d+\.\d+)/)
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
-# In order to pass notarization we need to sign any binaries and libraries included in the package.
-# This makes sure we include and bins and libs that are brought in by gems.
-semver = Gem::Version.create(version).segments
-ruby_mmv = "#{semver[0..1].join(".")}.0"
-ruby_dir = "#{install_dir}/embedded/lib/ruby/#{ruby_mmv}"
-gem_dir = "#{install_dir}/embedded/lib/ruby/gems/#{ruby_mmv}"
-bin_dirs bin_dirs.concat ["#{gem_dir}/gems/*/bin/**"]
-lib_dirs ["#{ruby_dir}/**", "#{gem_dir}/extensions/**", "#{gem_dir}/gems/*", "#{gem_dir}/gems/*/lib/**", "#{gem_dir}/gems/*/ext/**"]
-
 relative_path "ruby-#{version}"
 
 env = with_standard_compiler_flags(with_embedded_path)
